ad147bdb961ce64ef028cf2622b41a4d5511c964
Nícolas <nicolascinel@gmail.com>
2026-09-10 19:33:59 -0300

    feat: Adiciona Componentes de tarefas

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
 
-const TeskItem = () => {
+const TaskItem = ({ tarefa, concluirTarefa, removerTarefa }) => {
+
   return (
-    <div>
-      
-    </div>
+<div className="task-item">
+<h3>{tarefa.nome}</h3>
+<p>Data: {tarefa.data}</p>
+<p>{tarefa.descricao}</p>
+<p>Prioridade: {tarefa.prioridade}</p>
+<p>
+
+        Status: {tarefa.concluida ? 'Concluída' : 'Pendente'}
+</p>
+<button onClick={() => concluirTarefa(tarefa.id)}>
+
+        {tarefa.concluida ? 'Voltar para Pendente' : 'Concluir'}
+</button>
+<button onClick={() => removerTarefa(tarefa.id)}>
+
+        Remover
+</button>
+</div>
+
   )
+
 }
 
-export default TeskItem
+export default TaskItem
+ 
